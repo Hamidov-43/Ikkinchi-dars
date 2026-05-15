a89a9a5c8e9742f3d57adb245677b6e50d629d91
Hamidov Hurshedjon <hamidovx8gmail.com>
2026-05-15 22:24:50 +0500

    Set ()

--- index.py
+++ index.py
@@ -13,3 +13,25 @@ print("admin7" in admin_users)
 coordinates = (10.0, 20.0)
 print(type(coordinates))  # <class 'tuple'>
 print(coordinates)  # (10.0, 20.0)
+
+# Set example
+unique_numbers = {1,1, 2, 3, 4, 5}
+print(type(unique_numbers))  # <class 'set'>
+print(unique_numbers)  # {1, 2, 3, 4, 5}
+unique_numbers.add(6)
+print(unique_numbers)  # {1, 2, 3, 4, 5, 6}
+unique_numbers.remove(2)
+print(unique_numbers)  # {1, 3, 4, 5, 6}
+langs = set()
+langs.add("Python")
+langs.add("Java")      
+print(langs)  # {'Python', 'Java'}
+nums1 = {1, 2, 3}
+nums2 = {3, 4, 5}
+print(nums1 & nums2)  # {3}
+print(nums1 | nums2)  # {1, 2, 3, 4, 5}
+print(nums1 - nums2)  # {1, 2}
+print(nums2 - nums1)  # {4, 5}
+print(nums1 ^ nums2)  # {1, 2, 4, 5}
+set = {1, 2, 3, 4, 5}
+print(set)  # {1, 2, 3, 4, 5}
